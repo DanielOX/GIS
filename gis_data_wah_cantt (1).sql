98ceac0761a5191f5a677c213d16c1a4b5159b28
DanielOX <metalcool07@yahoo.com>
2018-04-17 07:47:21 -0700

    UPDATED SQL FILE WITH FULL DATABASE COLLATIONS

--- gis_data_wah_cantt (1).sql
+++ gis_data_wah_cantt (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2018 at 02:42 PM
+-- Generation Time: Apr 17, 2018 at 02:46 PM
 -- Server version: 5.7.14
 -- PHP Version: 7.0.10
 
@@ -19,6 +19,45 @@ SET time_zone = "+00:00";
 --
 -- Database: `gis_data_wah_cantt`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Education'),
+(2, 'Food'),
+(3, 'Banks'),
+(4, 'Health'),
+(5, 'Chemist'),
+(6, 'Places'),
+(7, 'Sationaries'),
+(8, 'Mosques'),
+(9, 'Offices'),
+(10, 'Stores');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -182,9 +221,372 @@ INSERT INTO `locations` (`id`, `name`, `general_type`, `description`, `lat`, `ln
 (140, 'UNIVERSITY OF WAH', 1, 'University of Wah is a private-sector higher education institution founded with the vision of achieving excellence in unbiased dissemination of quality education to all. It was granted Charter by the Punjab Provincial Assembly in 2009. University is offer', '33.7448013', '72.7866106'),
 (141, 'POF HOSPITAL', 4, 'POFs Hospital was established in 1962 with an initial capacity of 103 beds. Hospital capacity increased 200, 400 and 500 beds in 1963, 1969 and 1987 respectively. Existing bed strength is 676 due to introduction of new departments over successive years. I', '33.7446229', '72.77154729999999');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locationsx`
+--
+
+CREATE TABLE `locationsx` (
+  `id` int(11) NOT NULL,
+  `name` varchar(46) CHARACTER SET utf8 DEFAULT NULL,
+  `lat` decimal(8,6) DEFAULT NULL,
+  `lng` decimal(8,6) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `locationsx`
+--
+
+INSERT INTO `locationsx` (`id`, `name`, `lat`, `lng`) VALUES
+(1, 'FATIMA JINNAH AVENUE', '33.751930', '72.776084'),
+(2, 'LABAIK BAKERS', '33.751882', '72.776294'),
+(3, 'BROADWAY BAKERS', '33.752039', '72.776466'),
+(4, 'STUDENT BOOK CENTER', '33.753158', '72.774369'),
+(5, 'GYMNASIUM', '33.754116', '72.774191'),
+(6, 'POF WALFARE HALL', '33.754116', '72.774191'),
+(7, 'POF SWIMMING POOL', '33.755100', '72.772132'),
+(8, 'POF SPORTS BAORB', '33.754116', '72.774191'),
+(9, 'WALFARE FOOTBALL GROUND', '33.754613', '72.775026'),
+(10, 'JAMIYA MASJID MUHAMADI', '33.752376', '72.774204'),
+(11, 'ALMAKKAH SHOPING MART', '33.752816', '72.775520'),
+(12, 'IMAMBARGAH', '33.755841', '72.772677'),
+(13, 'JAMIYA MASJID WAH CANTT', '33.762383', '72.770608'),
+(14, 'POF WELFAER COMPUTER CENTER', '33.762176', '72.770665'),
+(15, 'AL-SAMAD RESTAURANT', '33.762243', '72.770747'),
+(16, 'UNIQUE BAKERS', '33.762341', '72.770566'),
+(17, 'ALLIDE BANK', '33.762341', '72.770566'),
+(18, 'NADRA REGISTEATION CENTER', '33.762734', '72.770549'),
+(19, 'PTCL OFFICE', '33.762806', '72.769895'),
+(20, 'PAKISTAN POST OFFICE', '33.763206', '72.769355'),
+(21, 'CONTONMENT BAORD OFFICE', '33.763853', '72.768734'),
+(22, 'ELECTRICAL COMPLAINT OFFICE', '33.764389', '72.769145'),
+(23, 'NMATIONAL BANK OF PAKISTAN', '33.764196', '72.768406'),
+(24, 'KEYANI RESTAURANT', '33.764354', '72.766811'),
+(25, 'UBL BANK', '33.764409', '72.768011'),
+(26, 'HBL BANK', '33.764748', '72.767818'),
+(27, 'AGHA KHAN UNIVERSITY', '33.764674', '72.767566'),
+(28, 'AGHA KHAN HOSPITAL', '33.764674', '72.767566'),
+(29, 'MCB BANK', '33.764693', '72.767627'),
+(30, 'CAPRI CONTINALTAL BAKERS', '33.765036', '72.767719'),
+(31, 'KHAWAJA MADICAL HALL', '33.765103', '72.767037'),
+(32, 'MCB BANK', '33.765135', '72.766987'),
+(33, 'MARHABA BAKERS', '33.765543', '72.766469'),
+(34, 'SUMMIT BANK', '33.765701', '72.766328'),
+(35, 'BANK OF PANJAB', '33.765729', '72.766179'),
+(36, 'PALACE BAKERS', '33.765815', '72.766044'),
+(37, 'NATIONAL SAVINGS BANK', '33.765916', '72.766049'),
+(38, 'KENTUCKET FAST FOOD', '33.766191', '72.765843'),
+(39, 'MOROSO P2P1', '33.766174', '72.765699'),
+(40, 'NASHEMAN SCHOOOL', '33.743395', '72.790494'),
+(41, 'MASHAL COLLAGE', '33.741547', '72.792452'),
+(42, 'WAH MEDICAL COLLAGE', '33.747713', '72.788596'),
+(43, 'POFIT COLLAGE', '33.745419', '72.784008'),
+(44, 'WAH ENGEENIRNG COLLAGE', '33.747848', '72.784953'),
+(45, 'CP PUBLIC SCHOOL', '33.747631', '72.753289'),
+(46, 'CB PUBLIC COLLAGE', '33.747631', '72.753289'),
+(47, 'SIRSYED SCHOOL', '33.755875', '72.782295'),
+(48, 'FG SCHOOL NO1 GIRLS', '33.751255', '72.777490'),
+(49, 'FG MIDDALE SCHOOL NO12', '33.751080', '72.773712'),
+(50, 'FG PUBLIC SCHOOL NO3', '33.751497', '72.772229'),
+(51, 'FG PUBLIC SCHOOL NO3 GIRLS', '33.757994', '72.766451'),
+(52, 'GOVT ANJUMAN ISLAMIA SCHOOL', '33.755402', '72.766887'),
+(53, 'FG PUBLIC SCHOOL NO1 BOYS', '33.763107', '72.768059'),
+(54, 'GOVT GILANI HIGH SCHOOL', '33.763205', '72.754828'),
+(55, 'FG PUBLIC SCHOOL NO 4 BOYS', '33.766060', '72.752575'),
+(56, 'FG PUBLIC NO 2 GIRLS', '33.768370', '72.744119'),
+(57, 'FG JUNIOR PUBLIC SCHOOL NO5', '33.767552', '72.760926'),
+(58, 'FG POST GRADUATE COLLAGE GIRLS', '33.769057', '72.761414'),
+(59, 'SIRSYED COLLAGE CAPUS 1', '33.772141', '72.761386'),
+(60, 'FG MODEL PUBLIC SCHOOL', '33.773624', '72.751670'),
+(61, 'FG DEGREE COLLEGE FOR MEN', '33.775286', '72.749661'),
+(62, 'FG SCHOOL NO4 GIRLS', '33.773668', '72.744332'),
+(63, 'FG JUNIOR SCHOOL NO 10', '33.780888', '72.737899'),
+(64, 'SIRSYED COLLAGE BOYS NO2', '33.781262', '72.737232'),
+(65, 'MARGALLA GRAMMER SCHOOL', '33.781155', '72.736044'),
+(66, 'ORGANON SCHOOL PRE NURSARY O,A LEVEL', '33.781880', '72.735178'),
+(67, 'LAHORE GRAMMER SCHOOL ', '33.782387', '72.734510'),
+(68, 'RAISE AND SHINE MONTESORI CAMPUS 1', '33.783077', '72.733579'),
+(69, 'MARGALLA GRAMMER SCHOOL ', '33.783357', '72.733146'),
+(70, 'WAH POLYTECHNIC COLLAGE', '33.784077', '72.730811'),
+(71, 'FAROOQI MAODEL HIGH SCHOOL MONTESORI CAMPUS', '33.787314', '72.726406'),
+(72, 'WAH GRAMMER SCHOOL SENIOR BRANCH FOR BOYS', '33.787480', '72.724628'),
+(73, 'FG PUBLIC SCHOOOL NO7 GIRLS', '33.784441', '72.721516'),
+(74, 'FG PUBLIC PUBLIC SCHOOL NO2 BOYS', '33.781756', '72.725047'),
+(75, 'HORIZON DEGREE COLLEGE FOR GIRLS', '33.785842', '72.729574'),
+(76, 'JINNAH COLLEGE', '33.785979', '72.729190'),
+(77, 'ROOTS INTERNATIONAL SCHOOL ', '33.786440', '72.728743'),
+(78, 'BAHRIA FOUNDATION CLOOEGE PRIMARY CAMPUS', '33.786913', '72.728058'),
+(79, 'RISE AND SHINE COLLAGE GIRLS CAMPUS', '33.787013', '72.727899'),
+(80, 'PUNJAB COLLEGE OF EXCELENCE GIRLS CUMPUS', '33.787723', '72.726880'),
+(81, 'PUNJAB COLLEGE OF EXCELENCE IN COMMERCE', '33.787884', '72.726627'),
+(82, 'THE CITY SCHOOL ', '33.788023', '72.726480'),
+(83, 'SP.PAUL HIGH SCHOOL', '33.778856', '72.719057'),
+(84, 'FAROOQI MAODEL HIGH SCHOOL BOYS CAMPUS', '33.788458', '72.727341'),
+(85, 'RISE AND SHINE COLLAGE GIRLS CAMPUS 2', '33.789710', '72.729879'),
+(86, 'RISE AND SHINE BOYS CAMPUS', '33.789563', '72.729816'),
+(87, 'ZORGITOONA SCHOOL', '33.789801', '72.730089'),
+(88, 'ST.PAUL HIGH SCHOOL GULSHAN KALOONI', '33.790664', '72.731976'),
+(89, 'NEW ERA SCHOOL SYSTEM', '33.791718', '72.734652'),
+(90, 'SIRSYED HIGH SCHOOL CAMPUS', '33.795210', '72.749411'),
+(91, 'FG JUNIOR PUBLIC SCHOOL GUDWAL', '33.797094', '72.748825'),
+(92, 'FG PUBLIC SCHOOL NO5 BOYS 26 AREA', '33.794991', '72.748343'),
+(93, 'CANTT COLLEGE FOR GIRLS', '33.788378', '72.727709'),
+(94, 'WAH COLLEGE OF ACCOUNTENCY', '33.788899', '72.727324'),
+(95, 'FATIMA JINNAH COLLEGE FOR WOMEN', '33.787598', '72.728065'),
+(96, 'FAROOQI MODEL HIGH SCHOOL GIRLS CAMPUS', '33.787313', '72.728476'),
+(97, 'ROOTS INTERNATIONAL SCHOOL OFFICER COLONY', '33.786706', '72.729323'),
+(98, 'FORBELS SCHOOL INTERNATIONAL SCHOOL WAH CAMPUS', '33.786362', '72.729820'),
+(99, 'BECON HOUSE SCHOOL JUNIOR BRANCH', '33.786174', '72.730079'),
+(100, 'AL-HUDA MONTESSORI SCHOOL THE MALL', '33.785501', '72.731130'),
+(101, 'BECON HOUSE SCHOOL WAH CANTT CAMPUS', '33.785390', '72.732031'),
+(102, 'LAHORE GRAMMER SCHOOL PLAYGROUP GRADE 2', '33.783283', '72.734248'),
+(103, 'LAHORE GRAMMER SCHOOL', '33.782057', '72.736385'),
+(104, 'MARGALLAH GREMMAR SCHOOL', '33.782622', '72.735229'),
+(105, 'THE SCHOLAR SCHOOL', '33.788909', '72.725401'),
+(106, 'WAH CITY SCHOOL', '33.789571', '72.724230'),
+(107, 'ROOTS SCHOOL MONTESORI LALARUKH', '33.789620', '72.724121'),
+(108, 'RAWALPHINDI COLLEGE OF COMMERCE AND SCIENCE', '33.789645', '72.724036'),
+(109, 'WIMS DEGREE COLLEGE ', '33.790653', '72.723630'),
+(110, 'THE KNOWLEDGE SCHOOL', '33.790801', '72.723426'),
+(111, 'EDUCATOR WAH CAPPUS', '33.791570', '72.722388'),
+(112, 'WCA AND SCIENCE GIRLS CAMPUS', '33.791790', '72.722100'),
+(113, 'THE COUNTRY SCHOOL LALAZAR CAMPUS', '33.791937', '72.721929'),
+(114, 'PICS MODEL SCHOOL AND MONTESORI SYSTEM', '33.792451', '72.721380'),
+(115, 'HOPE COLLEGE SCIENCE COMMERCE', '33.792517', '72.721266'),
+(116, 'WCA SCHOOLING SYSTEM PLAYGROUP TO TENTH', '33.791060', '72.721915'),
+(117, 'SCHOLAR SCIENCE COLLEGE GIRLS CAMPUS', '33.790777', '72.722252'),
+(118, 'BAHARIA FOUNDATION COLLEGE', '33.790722', '72.722500'),
+(119, 'BAHARIA COLLEGE HIGHER SECONDARY LEVEL', '33.790629', '72.722657'),
+(120, 'SARMAD BOOK PALACE', '33.766456', '72.753955'),
+(121, 'CORNER SWEETS AND BAKRES', '33.767030', '72.754730'),
+(122, 'CORNER BAKERS', '33.767266', '72.754469'),
+(123, 'CSD CANTEEN STORE DEPARTMENT', '33.767810', '72.755255'),
+(124, 'JAMIA TALEEMUNL QURAN', '33.767950', '72.754221'),
+(125, 'BHATTI BOOK DEPO', '33.768154', '72.754301'),
+(126, 'QASIR GENERAL STORE', '33.766288', '72.755197'),
+(127, 'SUPER SWEETS AND BAKERS', '33.766251', '72.755210'),
+(128, 'SMILE DENTAL CLINIC', '33.765493', '72.755332'),
+(129, 'HASHMI DUA KHANA', '33.765449', '72.755251'),
+(130, 'QADRI STORE AND CLINIC', '33.765313', '72.754989'),
+(131, 'SHAMA BAKERS', '33.765346', '72.754997'),
+(132, 'SAIFA MEDICOZ STORE', '33.765079', '72.754523'),
+(133, 'SIDQI CLINIC', '33.765136', '72.753926'),
+(134, 'AHSAN CLINIC AND LABORATORY', '33.765422', '72.753802'),
+(135, 'HUMYUR CLINIC', '33.765420', '72.753745'),
+(136, 'LIFE LINE PHARMACY AND CLINIC', '33.765441', '72.753659'),
+(137, 'ABRAR DENTAL CLINIC', '33.765458', '72.753142'),
+(138, 'WELFARE CLUB 2     8 AREA', '33.762639', '72.755639');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location_type`
+--
+
+CREATE TABLE `location_type` (
+  `id` int(11) NOT NULL,
+  `location_id` int(11) NOT NULL,
+  `general_type` int(11) NOT NULL,
+  `sub_type` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `location_type`
+--
+
+INSERT INTO `location_type` (`id`, `location_id`, `general_type`, `sub_type`) VALUES
+(3, 1, 1, 1),
+(4, 2, 1, 1),
+(5, 3, 1, 1),
+(6, 4, 1, 1),
+(7, 5, 1, 1),
+(8, 6, 1, 1),
+(9, 7, 1, 1),
+(10, 8, 1, 1),
+(11, 9, 1, 1),
+(12, 10, 1, 1),
+(13, 10, 1, 1),
+(14, 11, 1, 1),
+(15, 12, 1, 2),
+(16, 13, 1, 2),
+(17, 14, 1, 2),
+(18, 15, 1, 1),
+(19, 16, 1, 1),
+(20, 17, 1, 1),
+(21, 18, 1, 1),
+(22, 19, 1, 1),
+(23, 20, 1, 1),
+(24, 21, 1, 1),
+(25, 22, 1, 2),
+(26, 23, 1, 1),
+(27, 24, 1, 1),
+(28, 25, 1, 1),
+(29, 26, 1, 2),
+(30, 27, 1, 2),
+(31, 28, 1, 2),
+(32, 29, 1, 2),
+(33, 30, 1, 1),
+(34, 31, 1, 2),
+(35, 32, 1, 2),
+(36, 33, 1, 1),
+(37, 34, 1, 1),
+(38, 35, 1, 1),
+(39, 36, 1, 1),
+(40, 37, 1, 2),
+(41, 38, 1, 1),
+(42, 39, 1, 1),
+(43, 40, 1, 1),
+(44, 41, 1, 1),
+(45, 42, 1, 1),
+(46, 43, 1, 2),
+(47, 44, 1, 1),
+(48, 45, 1, 1),
+(49, 46, 1, 2),
+(50, 47, 1, 1),
+(51, 48, 1, 2),
+(52, 49, 1, 2),
+(53, 50, 1, 1),
+(54, 51, 1, 1),
+(55, 52, 1, 1),
+(56, 53, 1, 1),
+(57, 54, 1, 1),
+(58, 55, 1, 1),
+(59, 56, 1, 1),
+(60, 57, 1, 1),
+(61, 58, 1, 1),
+(62, 59, 1, 1),
+(66, 61, 1, 2),
+(65, 60, 1, 1),
+(67, 62, 1, 1),
+(68, 63, 1, 2),
+(69, 64, 1, 2),
+(70, 65, 1, 2),
+(71, 66, 1, 2),
+(72, 67, 1, 1),
+(73, 68, 2, 6),
+(74, 69, 2, 6),
+(75, 70, 3, 9),
+(76, 71, 2, 4),
+(77, 72, 3, 9),
+(78, 73, 3, 9),
+(79, 74, 2, 4),
+(80, 75, 3, 9),
+(81, 76, 4, 7),
+(82, 77, 2, 4),
+(83, 78, 3, 9),
+(84, 79, 4, 7),
+(85, 80, 1, 3),
+(86, 81, 3, 9),
+(87, 82, 3, 9),
+(88, 83, 2, 5),
+(89, 84, 3, 9),
+(90, 85, 9, 10),
+(91, 86, 9, 10),
+(92, 87, 9, 10),
+(93, 88, 9, 10),
+(94, 89, 9, 10),
+(95, 90, 3, 9),
+(96, 91, 2, 4),
+(97, 92, 2, 5),
+(98, 93, 6, 13),
+(99, 94, 8, 11),
+(100, 95, 8, 11),
+(101, 96, 10, 12),
+(102, 97, 8, 11),
+(103, 98, 6, 13),
+(104, 99, 6, 13),
+(105, 100, 6, 13),
+(106, 101, 6, 13),
+(107, 102, 6, 13),
+(108, 103, 7, 14),
+(109, 104, 2, 4),
+(110, 105, 2, 4),
+(111, 106, 6, 13),
+(112, 107, 1, 1),
+(113, 108, 1, 2),
+(114, 109, 1, 2),
+(115, 110, 1, 1),
+(116, 111, 1, 1),
+(117, 112, 1, 1),
+(118, 113, 1, 1),
+(119, 114, 1, 1),
+(120, 115, 1, 2),
+(121, 116, 1, 1),
+(122, 117, 1, 2),
+(123, 118, 1, 2),
+(124, 119, 1, 2),
+(125, 120, 7, 14),
+(126, 121, 2, 4),
+(127, 122, 2, 4),
+(128, 123, 10, 12),
+(129, 124, 1, 11),
+(130, 125, 7, 14),
+(131, 126, 10, 12),
+(132, 127, 2, 4),
+(133, 128, 4, 7),
+(134, 129, 4, 7),
+(135, 130, 10, 12),
+(136, 131, 2, 4),
+(137, 132, 10, 12),
+(138, 133, 4, 7),
+(139, 134, 4, 7),
+(140, 135, 4, 7),
+(141, 136, 4, 7),
+(142, 137, 4, 8),
+(143, 138, 6, 6),
+(144, 139, 1, 3),
+(145, 140, 1, 3),
+(146, 141, 4, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_categories`
+--
+
+CREATE TABLE `sub_categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `general_type` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sub_categories`
+--
+
+INSERT INTO `sub_categories` (`id`, `name`, `general_type`) VALUES
+(1, 'Schools', 1),
+(2, 'Colleges', 1),
+(3, 'Universities', 1),
+(4, 'Bakery', 2),
+(5, 'Restaurants', 2),
+(6, 'Fast Food', 2),
+(7, 'Hospital', 4),
+(8, 'Clinics', 4),
+(9, 'banks', 3),
+(10, 'office', 9),
+(11, 'Mosque', 8),
+(12, 'General Store', 10),
+(13, 'Famous Places', 6),
+(14, 'stationary', 7),
+(15, 'chemist', 5);
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `locations`
@@ -193,14 +595,57 @@ ALTER TABLE `locations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `locationsx`
+--
+ALTER TABLE `locationsx`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `location_type`
+--
+ALTER TABLE `location_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sub_categories`
+--
+ALTER TABLE `sub_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `locations`
 --
 ALTER TABLE `locations`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=142;
+--
+-- AUTO_INCREMENT for table `locationsx`
+--
+ALTER TABLE `locationsx`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=139;
+--
+-- AUTO_INCREMENT for table `location_type`
+--
+ALTER TABLE `location_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=147;
+--
+-- AUTO_INCREMENT for table `sub_categories`
+--
+ALTER TABLE `sub_categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
